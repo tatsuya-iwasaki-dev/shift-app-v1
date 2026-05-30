79cf4ed6ad050cbc2907aa80817d085eaa06ce47
Tatsuya Iwasaki <iwasakitatsuya9@gmail.com>
2026-05-31 01:23:19 +0900

    バリデーション追加

--- main.js
+++ main.js
@@ -4,7 +4,20 @@ button.addEventListener('click', function() {
   const name = document.getElementById('name-select').value;
   const date = document.getElementById('date').value;
   const shiftRadio = document.querySelector('input[name="shift"]:checked');
-  const shift = shiftRadio ? shiftRadio.value : "未選択";
+  const shift = shiftRadio ? shiftRadio.value : null;
+
+  if (name === '') {
+    alert('名前を選択してください');
+    return;
+  }
+  if (date === '') {
+    alert('日付を選択してください');
+    return;
+  }
+  if (shift === null) {
+    alert('勤務時間を選択してください');
+    return;
+  }
 
   document.getElementById('result').textContent = name + 'さん/' + date + '/' + shift;
 });
